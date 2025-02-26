db460340c8d4c0cf13e75d2f4d3cff57b789c527
Sarthak <saryatnam22@gmail.com>
2025-02-27 00:46:04 +0530

    PostMapping

--- src/main/java/com/sarthak/FirstSpringBoot/HelloController.java
+++ src/main/java/com/sarthak/FirstSpringBoot/HelloController.java
@@ -1,13 +1,22 @@
 package com.sarthak.FirstSpringBoot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController         //  Specialized version of @Controller annotation, that this is a RESTful service
+@RestController         //  Specialized version of @Controller annotation, that this is a RESTful service. It means that every method in the class will return data directly as a JSON or XML response instead of rendering a view. It specifies that we will be defining all of the endpoints in this class.
 public class HelloController {
 
     @GetMapping("/hello")           // if any request comes under domainName/helo, spring will execute this method
     public String hello() {
         return "Hello World";
     }
+
+    @PostMapping("/hello")
+    public String helloPost(@RequestBody String name) {     //RequestBody is used to map a HTTP request over a java object.
+        return "Hello "+name+" !";
+    }
 }
+
+//  A controller is responsible for handling incoming HTTP responses , processing them with the help of a service layer, and returning responses. It acts as a intermediary between the client and application's business logic
