806602d48cff55df8afeaab5266f2bcfc147471f
Sarthak <saryatnam22@gmail.com>
2025-02-26 13:42:04 +0530

    hello world REST Api

--- src/main/java/com/sarthak/FirstSpringBoot/HelloController.java
+++ src/main/java/com/sarthak/FirstSpringBoot/HelloController.java
@@ -1,8 +1,13 @@
 package com.sarthak.FirstSpringBoot;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController         //  Specialized version of @Controller annotation, that this is a RESTful service
 public class HelloController {
 
+    @GetMapping("/hello")           // if any request comes under domainName/helo, spring will execute this method
+    public String hello() {
+        return "Hello World";
+    }
 }
